3239cea048b1ea34ef310e006d51d7625e5c36c8
GoodEatz <kenmytruong.94@gmail.com>
2017-10-30 18:56:00 -0700

    Test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CS at CSUEB' });
+  res.render('index', { title: 'Kenny at CSUEB' });
 });
 
 module.exports = router;
